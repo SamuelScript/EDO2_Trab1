831d020758256f498bec4997a1fdf69e595ea9f8
gabriel <gabrielmoraesmanhaes@hotmail.com>
2023-11-27 12:32:08 -0300

    Mais implementação da interface gráfica.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -6,15 +6,12 @@ import javax.swing.*;
 
 public class Main {
     //static double length_x = 50;
-    //static double length_xf = length_x*0.5;
     //static double a = 1;
     //static double t_max = 70;
-    //static double t_int = t_max*0.5;
     //static double Ca = 20;
     //static double Cb = 20;
     //static double Cc = 0;
     //static final int s_partition = 1000;
-    //static final int draw = 1;
 
     public static void main(String[] args) {
         try {
--- src/ui/MainFrame.java
+++ src/ui/MainFrame.java
@@ -1,13 +1,14 @@
 package ui;
 
 import javax.swing.*;
-import javax.swing.event.TableModelListener;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumnModel;
-import javax.swing.table.TableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Arrays;
 
 public class MainFrame extends JFrame {
     private JPanel contentPane;
@@ -19,6 +20,26 @@ public class MainFrame extends JFrame {
     private JPanel panelMethods;
     private JScrollPane scrollPaneMethods;
     private JTable tableMethods;
+    private JPanel panelHeader;
+    private JLabel lblXLength;
+    private JTextField textFieldLengthX;
+    private JTextField textFieldAlpha;
+    private JLabel lblAlpha;
+    private JLabel lblTMax;
+    private JTextField textFieldTMax;
+    private JLabel lblCLeft;
+    private JTextField textFieldCLeft;
+    private JLabel lblCRight;
+    private JTextField textFieldTRight;
+    private JLabel lblTIni;
+    private JTextField textFieldTIni;
+    private JLabel lblUnitLenghtX;
+    private JLabel lblUnitAlpha;
+    private JComboBox comboBoxUnitTemp1;
+    private JComboBox comboBoxUnitTemp2;
+    private JComboBox comboBoxUnitTemp3;
+    private JLabel lblStatus;
+    private JScrollPane scrollPaneMethodInfo;
     private int currentCard = 1;
 
     public MainFrame() {
@@ -28,7 +49,9 @@ public class MainFrame extends JFrame {
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        DefaultTableModel methodsModel = new DefaultTableModel(new Object[][] {{"Metodo 1", false}, {"Metodo 2", true}, {"Melhor metodo", false}}, new String[] {"Metodo", "Selecionar"}) {
+        DefaultTableModel methodsModel = new DefaultTableModel(new Object[][] {
+                {"Metodo 1", false}, {"Metodo 2", false}, {"Melhor metodo", false}},
+                new String[] {"Metodo", "Selecionar"}) {
             final Class<?>[] columnTypes = new Class[] {String.class, Boolean.class};
             public Class<?> getColumnClass(int columnIndex) {return columnTypes[columnIndex];}
         };
@@ -38,6 +61,15 @@ public class MainFrame extends JFrame {
         methodsColumnModel.getColumn(0).setResizable(false);
         methodsColumnModel.getColumn(1).setResizable(false);
         methodsColumnModel.getColumn(1).setMaxWidth(100);
+        tableMethods.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+        ListSelectionModel selectionModel = tableMethods.getSelectionModel();
+
+        selectionModel.addListSelectionListener(new ListSelectionListener() {
+            public void valueChanged(ListSelectionEvent e) {
+                lblStatus.setText(String.valueOf(tableMethods.getSelectedRow()));
+            }
+        });
         anteriorButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
